ed29c38049b6932ffd01cd82bb6013231a2a9c50
Amine Ben hammou <webneat@gmail.com>
2015-09-24 02:18:35 +0100

    TravisCI fixed

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # installing dependencies for lumen-generators
 composer install --no-interaction
+composer update --no-interaction
 
 # installing dependencies for lumen-test
 cd lumen-test && composer install --no-interaction
