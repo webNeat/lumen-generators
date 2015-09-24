4e6b3999c2af2f38e31ff49e90858ca24c75015e
Amine Ben hammou <webneat@gmail.com>
2015-09-24 02:03:29 +0100

    TravisCI added

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# installing dependencies for lumen-generators
+composer install --no-interaction
+
+# installing dependencies for lumen-test
+cd lumen-test && composer install --no-interaction
---
+++ test.sh
@@ -0,0 +1,4 @@
+# Runing tests for Lumen
+cd lumen-test
+wget http://codeception.com/codecept.phar
+php codecept.phar run
