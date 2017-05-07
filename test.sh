b22b59ab5d2518e5fb78e48988c01618235a48f6
Stéphane <sbour@welcome-media.fr>
2017-05-07 19:40:18 +0200

    perf: detect if codecept exist

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+
 # Runing tests for Lumen
-cd lumen-test
-wget http://codeception.com/codecept.phar
+cd lumen-test || return
+
+if [ ! -f codecept.phar ]; then
+    wget http://codeception.com/codecept.phar
+fi
 php codecept.phar run
