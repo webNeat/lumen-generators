a885b69cc45d0ac9d7d03cf98fd9e6b545c996e2
Khalid Hisham <timoncr9@gmail.com>
2020-01-29 17:54:59 +0200

    Update RouteCommand.php
    Fixing string methods to be compatible with lumen 6

--- src/Commands/RouteCommand.php
+++ src/Commands/RouteCommand.php
@@ -68,7 +68,7 @@ class RouteCommand extends BaseCommand {
     {
         $controller = $this->option('controller');
         if(! $controller){
-            $controller = ucwords(str_plural(camel_case($this->argument('resource')))) . 'Controller';
+            $controller = ucwords(\Illuminate\Support\Str::plural(\Illuminate\Support\Str::camel($this->argument('resource')))) . 'Controller';
         }
         return $controller;
     }
